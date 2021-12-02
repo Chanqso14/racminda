31d9a2a619116e27d880e6d90f79bbd0b6160b4c
Chanqso14 <borisi.chankseliani.1@btu.edu.ge>
2021-12-02 20:20:16 +0400

    bug

--- app/src/main/java/com/example/racminda/MainActivity.kt
+++ app/src/main/java/com/example/racminda/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.racminda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView = findViewById<BottomNavigationView>(R.id.btmnv)
+        val controller = findNavController(R.id.nav_host_fragment)
+        val appconfig = AppBarConfiguration(setOf(
+            R.id.home,
+            R.id.notifications,
+            R.id.dashboard,
+            R.id.safule
+
+
+        ))
+        setupActionBarWithNavController(controller, appconfig)
+        navView.setupWithNavController(controller)
     }
 }
